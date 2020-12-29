414bfad6e493363c935087cb873a434d9b6edf72
Yener ÇİFTÇİ <ax22023@renault.com.tr>
2020-12-29 11:08:14 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# FAQ Plus App Template
+
+| [Documentation](https://github.com/OfficeDev/microsoft-teams-faqplusplus-app/wiki) | [Deployment guide](https://github.com/OfficeDev/microsoft-teams-faqplusplus-app/wiki/Deployment-guide) | [Architecture](https://github.com/OfficeDev/microsoft-teams-faqplusplus-app/wiki/Solution-overview) |
+| ---- | ---- | ---- |
+
+Chatbots are an easy way to provide answers to frequently asked questions by users. However, most chatbots fail to engage with users in a meaningful way because there is no human in the loop when the chatbot fails.  
+
+FAQ Plus bot is a friendly Q&A bot that brings a human in the loop when it is unable to help. One can ask the bot a question and the bot responds with an answer if it's in the knowledge base. If not, the bot allows the user to submit a query which then gets posted in a pre-configured team of experts who are help to provide support by acting upon the notifications from within their Team itself. 
+
+An end-user interacting with FAQ Plus:
+
+![FAQ Plus in action (user view)](https://github.com/OfficeDev/microsoft-teams-faqplusplus-app/wiki/images/FAQPlusEndUser.gif)
+
+Experts team using FAQ Plus:
+
+![FAQ Plus in action (experts view)](https://github.com/OfficeDev/microsoft-teams-faqplusplus-app/wiki/images/FAQPlusExperts.gif)
+
+## Legal Notice
+Please read the license terms applicable to this app template [here](https://github.com/OfficeDev/microsoft-teams-faqplusplus-app/blob/master/LICENSE). In addition to these terms, by using this app template you agree to the following:
+
+* You are responsible for complying with applicable privacy and security regulations related to use, collection and handling of any personal data by your app.  This includes complying with all internal privacy and security policies of your organization if your app is developed to be sideloaded internally within your organization.
+
+* Microsoft will have no access to data collected through your app.  Microsoft will not be responsible for any data related incidents or data subject requests.
+
+* Any trademarks or registered trademarks of Microsoft in the United States and/or other countries and logos included in this repository are the property of Microsoft, and the license for this project does not grant you rights to use any Microsoft names, logos or trademarks outside of this repository.  Microsoft’s general trademark guidelines can be found [here](https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks/usage/general.aspx)
+
+* Use of this template does not guarantee acceptance of your app to the Teams app store.  To make this app available in the Teams app store, you will have to comply with [submission process and validation](https://docs.microsoft.com/en-us/microsoftteams/platform/concepts/deploy-and-publish/appsource/publish), and all associated requirements such as including your own privacy statement and terms of use for your app. 
+
+
+## Getting started
+
+Begin with the [Solution overview](https://github.com/OfficeDev/microsoft-teams-faqplusplus-app/wiki/Solution-overview) to read about what the app does and how it works.
+
+When you're ready to try out FAQ Plus, or to use it in your own organization, follow the steps in the [Deployment guide](https://github.com/OfficeDev/microsoft-teams-faqplusplus-app/wiki/Deployment-guide).
+
+## Feedback
+
+Thoughts? Questions? Ideas? Share them with us on [Teams UserVoice](https://microsoftteams.uservoice.com/forums/555103-public)!
+
+Please report bugs and other code issues [here](https://github.com/OfficeDev/microsoft-teams-faqplusplus-app/issues/new).
+
+## Contributing
+
+This project welcomes contributions and suggestions.  Most contributions require you to agree to a
+Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
+the rights to use your contribution. For details, visit https://cla.microsoft.com.
+
+When you submit a pull request, a CLA-bot will automatically determine whether you need to provide
+a CLA and decorate the PR appropriately (e.g., label, comment). Simply follow the instructions
+provided by the bot. You will only need to do this once across all repos using our CLA.
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
+For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
+contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
---
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus.Configuration/Controllers/HomeController.cs
@@ -0,0 +1,272 @@
+﻿// <copyright file="HomeController.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.FAQPlusPlus.Configuration.Controllers
+{
+    using System.Net;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
+    using Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker;
+    using Microsoft.Teams.Apps.FAQPlusPlus.Common.Models;
+    using Microsoft.Teams.Apps.FAQPlusPlus.Common.Providers;
+
+    /// <summary>
+    /// Home Controller
+    /// </summary>
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ConfigurationProvider configurationPovider;
+        private readonly IQnAMakerClient qnaMakerClient;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomeController"/> class.
+        /// </summary>
+        /// <param name="configurationPovider">configurationPovider DI.</param>
+        /// <param name="qnaMakerClient">qnaMakerClient DI.</param>
+        public HomeController(ConfigurationProvider configurationPovider, IQnAMakerClient qnaMakerClient)
+        {
+            this.configurationPovider = configurationPovider;
+            this.qnaMakerClient = qnaMakerClient;
+        }
+
+        /// <summary>
+        /// The landing page
+        /// </summary>
+        /// <returns>Default landing page view</returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        /// <summary>
+        /// Parse team Id from first and then proceed to save it on success
+        /// </summary>
+        /// <param name="teamId">teamId is the unique string</param>
+        /// <returns>View</returns>
+        [HttpPost]
+        public async Task<ActionResult> ParseAndSaveTeamIdAsync(string teamId)
+        {
+            string teamIdAfterParse = this.ParseTeamIdFromDeepLink(teamId);
+            if (!string.IsNullOrEmpty(teamIdAfterParse))
+            {
+                return await this.SaveOrUpdateTeamIdAsync(teamIdAfterParse);
+            }
+            else
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "The provided team ID is not valid.");
+            }
+        }
+
+        /// <summary>
+        /// Save or update teamId in table storage which is received from View
+        /// </summary>
+        /// <param name="teamId">teamId is the unique deep link URL string associated with each team</param>
+        /// <returns>View</returns>
+        [HttpPost]
+        public async Task<ActionResult> SaveOrUpdateTeamIdAsync(string teamId)
+        {
+            bool saved = await this.configurationPovider.SaveOrUpdateEntityAsync(teamId, ConfigurationEntityTypes.TeamId);
+            if (saved)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            else
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, "Sorry, unable to save the team ID due to an internal error. Try again.");
+            }
+        }
+
+        /// <summary>
+        /// Get already saved team Id from table storage
+        /// </summary>
+        /// <returns>Team Id</returns>
+        [HttpGet]
+        public async Task<string> GetSavedTeamIdAsync()
+        {
+            return await this.configurationPovider.GetSavedEntityDetailAsync(ConfigurationEntityTypes.TeamId);
+        }
+
+        /// <summary>
+        /// Save or update knowledgeBaseId in table storage which is received from View
+        /// </summary>
+        /// <param name="knowledgeBaseId">knowledgeBaseId is the unique string knowledge Id</param>
+        /// <returns>View</returns>
+        public async Task<ActionResult> SaveOrUpdateKnowledgeBaseIdAsync(string knowledgeBaseId)
+        {
+            bool saved = await this.configurationPovider.SaveOrUpdateEntityAsync(knowledgeBaseId, ConfigurationEntityTypes.KnowledgeBaseId);
+            if (saved)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            else
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, "Sorry, unable to save the knowledge base ID due to an internal error. Try again.");
+            }
+        }
+
+        /// <summary>
+        /// Validate knowledge base Id from QnA Maker service first and then proceed to save it on success.
+        /// The QnA Maker endpoint key is also refreshed as part of this process.
+        /// </summary>
+        /// <param name="knowledgeBaseId">knowledgeBaseId is the unique string knowledge Id</param>
+        /// <returns>View</returns>
+        [HttpPost]
+        public async Task<ActionResult> ValidateAndSaveKnowledgeBaseIdAsync(string knowledgeBaseId)
+        {
+            bool isValidKnowledgeBaseId = await this.IsKnowledgeBaseIdValid(knowledgeBaseId);
+            if (isValidKnowledgeBaseId)
+            {
+                var endpointRefreshStatus = await this.RefreshQnAMakerEndpointKeyAsync();
+                if (!endpointRefreshStatus)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, "Sorry, unable to save the QnAMaker endpoint key due to an internal error. Try again.");
+                }
+
+                return await this.SaveOrUpdateKnowledgeBaseIdAsync(knowledgeBaseId);
+            }
+            else
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "The provided knowledge base ID is not valid.");
+            }
+        }
+
+        /// <summary>
+        /// Get already saved knowledge base Id from table storage
+        /// </summary>
+        /// <returns>knowledge base Id</returns>
+        [HttpGet]
+        public async Task<string> GetSavedKnowledgeBaseIdAsync()
+        {
+            return await this.configurationPovider.GetSavedEntityDetailAsync(ConfigurationEntityTypes.KnowledgeBaseId);
+        }
+
+        /// <summary>
+        /// Save or update welcome message to be used by bot in table storage which is received from View
+        /// </summary>
+        /// <param name="welcomeMessage">welcomeMessage</param>
+        /// <returns>View</returns>
+        [HttpPost]
+        public async Task<ActionResult> SaveWelcomeMessageAsync(string welcomeMessage)
+        {
+            bool saved = await this.configurationPovider.SaveOrUpdateEntityAsync(welcomeMessage, ConfigurationEntityTypes.WelcomeMessageText);
+            if (saved)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            else
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, "Sorry, unable to save the welcome message due to an internal error. Try again.");
+            }
+        }
+
+        /// <summary>
+        /// Get already saved Welcome message from table storage
+        /// </summary>
+        /// <returns>Welcome message</returns>
+        public async Task<string> GetSavedWelcomeMessageAsync()
+        {
+            var welcomeText = await this.configurationPovider.GetSavedEntityDetailAsync(ConfigurationEntityTypes.WelcomeMessageText);
+            if (welcomeText.Equals(string.Empty))
+            {
+                await this.SaveWelcomeMessageAsync(Strings.DefaultWelcomeMessage);
+            }
+
+            return await this.configurationPovider.GetSavedEntityDetailAsync(ConfigurationEntityTypes.WelcomeMessageText);
+        }
+
+        /// <summary>
+        /// Save or update help tab text to be used by bot in table storage which is received from View
+        /// </summary>
+        /// <param name="helpTabText">help tab text</param>
+        /// <returns>View</returns>
+        [HttpPost]
+        public async Task<ActionResult> SaveHelpTabTextAsync(string helpTabText)
+        {
+            bool saved = await this.configurationPovider.SaveOrUpdateEntityAsync(helpTabText, ConfigurationEntityTypes.HelpTabText);
+            if (saved)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            else
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, "Sorry, unable to save the help tab text due to an internal error. Try again.");
+            }
+        }
+
+        /// <summary>
+        /// Get already saved help tab message from table storage
+        /// </summary>
+        /// <returns>Help tab text</returns>
+        public async Task<string> GetSavedHelpTabTextAsync()
+        {
+            var helpText = await this.configurationPovider.GetSavedEntityDetailAsync(ConfigurationEntityTypes.HelpTabText);
+            if (helpText.Equals(string.Empty))
+            {
+                await this.SaveHelpTabTextAsync(Strings.DefaultHelpTabText);
+            }
+
+            return await this.configurationPovider.GetSavedEntityDetailAsync(ConfigurationEntityTypes.HelpTabText);
+        }
+
+        /// <summary>
+        /// Based on deep link URL received find team id and return it to that it can be saved
+        /// </summary>
+        /// <param name="teamIdDeepLink">team Id deep link</param>
+        /// <returns>team Id as string</returns>
+        private string ParseTeamIdFromDeepLink(string teamIdDeepLink)
+        {
+            // team id regex match
+            // for a pattern like https://teams.microsoft.com/l/team/19%3a64c719819fb1412db8a28fd4a30b581a%40thread.tacv2/conversations?groupId=53b4782c-7c98-4449-993a-441870d10af9&tenantId=72f988bf-86f1-41af-91ab-2d7cd011db47
+            // regex checks for 19%3a64c719819fb1412db8a28fd4a30b581a%40thread.tacv2
+            var match = Regex.Match(teamIdDeepLink, @"teams.microsoft.com/l/team/(\S+)/");
+
+            if (!match.Success)
+            {
+                return string.Empty;
+            }
+
+            return HttpUtility.UrlDecode(match.Groups[1].Value);
+        }
+
+        /// <summary>
+        /// Check if provided knowledgebase Id is valid or not.
+        /// </summary>
+        /// <param name="knowledgeBaseId">knowledge base id</param>
+        /// <returns><see cref="Task"/> boolean value indicating provided knowledgebase Id is valid or not</returns>
+        private async Task<bool> IsKnowledgeBaseIdValid(string knowledgeBaseId)
+        {
+            try
+            {
+                var kbIdDetail = await this.qnaMakerClient.Knowledgebase.GetDetailsAsync(knowledgeBaseId);
+                return kbIdDetail.Id == knowledgeBaseId;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Update the saved endpoint key
+        /// </summary>
+        /// <returns>Tracking task</returns>
+        private async Task<bool> RefreshQnAMakerEndpointKeyAsync()
+        {
+            try
+            {
+                var endpointKeys = await this.qnaMakerClient.EndpointKeys.GetKeysAsync();
+                await this.configurationPovider.SaveOrUpdateEntityAsync(endpointKeys.PrimaryEndpointKey, ConfigurationEntityTypes.QnAMakerEndpointKey);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
